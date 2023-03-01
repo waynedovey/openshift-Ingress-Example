05972a9fd47b41aedca576fb444a2ccb7e6a90e2
Wayne Dovey <wdovey@gmail.com>
2023-03-02 10:38:59 +1100

    Update create-project.sh

--- create-project.sh
+++ create-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 oc new-project tektutor
-oc create deployment nginx --image=bitnami/nginx:latest
+oc create deployment nginx --image=quay.io/wdovey/nginx:latest
 oc scale deploy/nginx --replicas=3
 oc expose deploy/nginx --port=8080
 
